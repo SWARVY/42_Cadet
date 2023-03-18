029803b6aaceece50b7159aafea89428659bfd46
SWARVY <swarvy0826@naver.com>
2023-03-18 15:04:23 +0900

    230318 save

--- Libft/ft_bzero.c
+++ Libft/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: hyeoshin <hyeoshin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 20:19:27 by sinhyeonho        #+#    #+#             */
-/*   Updated: 2023/03/13 22:49:51 by hyeoshin         ###   ########.fr       */
+/*   Updated: 2023/03/18 15:03:34 by hyeoshin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@
 
 void	ft_bzero(void *s, size_t size)
 {
-	ft_memset(s, 9, size);
+	ft_memset(s, 0, size);
 }
