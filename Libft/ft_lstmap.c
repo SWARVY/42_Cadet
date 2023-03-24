dbe822fd1e82fd2eb96beaecf848584671b879cf
SWARVY <swarvy0826@naver.com>
2023-03-24 15:21:58 +0900

    edit : 보너스 구현 완료

--- Libft/ft_lstmap.c
+++ Libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: hyeoshin <hyeoshin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 19:23:38 by hyeoshin          #+#    #+#             */
-/*   Updated: 2023/03/24 13:51:36 by hyeoshin         ###   ########.fr       */
+/*   Updated: 2023/03/24 15:21:21 by hyeoshin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,23 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*dupl_lst;
 	t_list	*tmp;
+	void	*t_content;
 
 	if (!lst || !f || !del)
 		return (0);
-	ft_lstiter(lst, f);
-	tmp = lst;
-	while (tmp)
+	dupl_lst = 0;
+	while (lst)
 	{
-		dupl_lst = ft_lstnew(tmp->content);
-		ft_lstadd_back(&dupl_lst, tmp->content);
-		tmp = tmp->content;
+		t_content = (*f)(lst->content);
+		tmp = ft_lstnew(t_content);
+		if (!tmp)
+		{
+			del(t_content);
+			ft_lstclear(&dupl_lst, del);
+			return (0);
+		}
+		ft_lstadd_back(&dupl_lst, tmp);
+		lst = lst->next;
 	}
+	return (dupl_lst);
 }
